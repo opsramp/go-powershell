d6cc57ca2abba06b58ce3ceadd3c20f3d14c6e86
OpsRampDeveloper <40623644+opsrampdeveloper@users.noreply.github.com>
2020-06-19 10:57:34 +0530

    changes for 40a

--- shell.go
+++ shell.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gorillalabs/go-powershell/backend"
-	"github.com/gorillalabs/go-powershell/utils"
+	"github.com/40a/go-powershell/backend"
+	"github.com/40a/go-powershell/utils"
 	"github.com/juju/errors"
 )
 
